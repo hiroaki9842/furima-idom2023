91e6924629d3994a8a447843beee3fe0a4a2833b
fumiya-yoshii <135097414+fumiya-yoshii@users.noreply.github.com>
2023-07-28 18:32:03 +0900

    userテーブルの設計

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,20 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
+  validates :nick_name, presence: true
+  validates :email, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :encrypted_password, presence: true,length:{minimum:6},format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
+  # validates :password, presence: true,length:{minimum:6}
+
+  validates :kanji_first_name, presence: true
+  validates :kanji_last_name, presence: true
+  validates :kana_first_name, presence: true
+  validates :kana_last_name, presence: true
+  validates :birthday_yyyy, presence: true
+  validates :birthday_mm, presence: true
+  validates :birthday_dd, presence: true
+
+
 end
--- db/migrate/20230728014634_devise_create_users.rb
+++ db/migrate/20230728014634_devise_create_users.rb
@@ -4,9 +4,17 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
 
+      t.string :email,              null: false,unique: true
+      t.string :encrypted_password, null: false
+      t.string :nick_name,null: false
+      t.string :kanji_first_name,null:false
+      t.string :kanji_last_name,null:false
+      t.string :kana_first_name,null:false
+      t.string :kana_last_name,null:false
+      t.integer :birthday_yyyy,null:false
+      t.integer :birthday_mm,null:false
+      t.integer :birthday_dd,null:false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
