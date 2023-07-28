f3510c1210fbdae82d597176cb8da80828e9b03b
shoei555 <masahide.takakura@glv.co.jp>
2023-07-28 11:27:48 +0900

    rspecの導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth   #Basic認証機能を有効化
 
   private
-  
+
   def basic_auth  #Basic認証機能の実装
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数でname/passを設定
     end
   end
 end
+
