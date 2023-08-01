8714552ada42da04c9cbf45e97313b671bf787b6
hiroaki9842 <137044356+hiroaki9842@users.noreply.github.com>
2023-08-01 16:49:40 +0900

    出品データの保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,23 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @category = Category.all
-    @condition = Condition.all
-    @shipping_cost = ShippingCost.all
-    @prefecture = Prefecture.all
-    @day_to_shipment = DayToShipment.all
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :image, :item_summary, :item_category_id, :item_condition_id, :shipping_cost_id, :prefecture_id, :day_to_shipment_id, :price)
+    .merge(user_id: current_user.id)
   end
 
 end
