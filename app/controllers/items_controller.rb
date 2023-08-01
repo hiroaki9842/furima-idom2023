67eb1393c44bbd18f30fdc5e8b5510b4e8721948
hiroaki9842 <137044356+hiroaki9842@users.noreply.github.com>
2023-08-01 12:30:05 +0900

    5つのプルダウン作成完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,13 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @category = Category.all
+    @condition = Condition.all
+    @shipping_cost = ShippingCost.all
+    @prefecture = Prefecture.all
+    @day_to_shipment = DayToShipment.all
+  end
+
 end
