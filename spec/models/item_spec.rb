0bffce7e07ff32d02e7186f7f7c24b9ab2a98bd7
AizawaAkitoshi <137147889+AizawaAkitoshi@users.noreply.github.com>
2023-08-03 19:47:35 +0900

    プルリクの返答#2

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -60,7 +60,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
         end
-
+        it 'ユーザーが紐づいていないと登録できない' do
+          @item.user = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include('User must exist')
+        end
         it 'カテゴリーが空だと出品ができない'do
           @item.item_category_id = "1"
           @item.valid?
