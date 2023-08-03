7ed8f696a754cfe9e80e481a272d4bc327ad8470
AizawaAkitoshi <137147889+AizawaAkitoshi@users.noreply.github.com>
2023-08-03 17:40:34 +0900

    5つのプルダウン

---
+++ app/models/day_to_shipment.rb
@@ -0,0 +1,9 @@
+class DayToShipment < ActiveHash::Base
+  self.data = [
+    #出品機能時の発送までの日数
+    { id: 1, name: '---' },
+    { id: 2, name: '1~2で発送' },
+    { id: 3, name: '2~3日で発送' },
+    { id: 4, name: '4~7日で発送' }
+    ]
+end
