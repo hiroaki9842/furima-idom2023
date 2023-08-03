4ae5fae8ed44d335fac6e8e2fa8f312cd5f6ac5d
hiroaki9842 <137044356+hiroaki9842@users.noreply.github.com>
2023-08-03 16:40:53 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def basic_auth  #Basic認証機能の実装
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数でname/passを設定
+      username == 'mita-jiro' && password == '263150' # 環境変数でname/passを設定
     end
   end
 end
