21de2788d657603802c86ae9cb6efb641ee80906
hiroaki9842 <137044356+hiroaki9842@users.noreply.github.com>
2023-08-07 14:46:44 +0900

    プルリク訂正#1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,9 @@ class ItemsController < ApplicationController
   # ログイン状態の確認
   before_action :authenticate_user!, except: [:index, :show]
 
+  #item_idからレコード取得
+  before_action :set_item, only: [:show,:edit,:update]
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -20,20 +23,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
     @condition = Condition.find(@item.item_condition_id).name
     @category = Category.find(@item.item_category_id).name
   end
 
   def edit
-    @item = Item.find(params[:id])
     if current_user != @item.user
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -45,7 +45,11 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:item_name, :image, :item_summary, :item_category_id, :item_condition_id, :shipping_cost_id,
-                                 :prefecture_id, :day_to_shipment_id, :price)
+                                  :prefecture_id, :day_to_shipment_id, :price)
           .merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
