2f0231aaf18953c61cb8aa92c19b98885d389d55
hiroaki9842 <137044356+hiroaki9842@users.noreply.github.com>
2023-08-08 20:42:43 +0900

    購入画面エラーハンドリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
   def index
       gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       @item = Item.find(params[:item_id])
-      if user_signed_in? && current_user.id == @item.user_id
+      if (user_signed_in? && current_user.id == @item.user_id) || @item.order != nil
         redirect_to root_path
       end
       @order_address = OrderAddress.new
