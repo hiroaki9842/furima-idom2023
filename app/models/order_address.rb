49ebef692c8f5936dabc1d92e4cfca76aef207c9
hiroaki9842 <137044356+hiroaki9842@users.noreply.github.com>
2023-08-08 20:42:21 +0900

    addressモデルのバリデーション

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,8 +4,12 @@ class OrderAddress
 
 #addressモデルのバリデーション
 with_options presence: true do
+  validates  :token
   validates  :post_code,      format:{with: /\A\d{3}[-]\d{4}\z/}
-  validates  :phone_number,   format:{with: /\A\d{10,11}\z/}
+  validates  :prefecture_id,   numericality: { other_than: 1 }
+  validates  :mailing_address
+  validates  :house_number
+  validates  :phone_number,   format:{with: /\A\d{10,11}\z/, message: 'is too short'}
   validates  :user_id
 end
 
