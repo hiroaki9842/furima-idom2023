d20b8f9257b467d6c11fe30218f62a38becfa753
hiroaki9842 <137044356+hiroaki9842@users.noreply.github.com>
2023-08-08 15:27:57 +0900

    order_addressモデル作成

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,15 +1,17 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :post_code,:prefecture_id,:mailing_address,:house_number,:building_name,:phone_number,
-                :user_id,:item_id
+  attr_accessor :post_code,:prefecture_id,:mailing_address,:house_number,:building_name,:phone_number,:user_id,:item_id
 
 #addressモデルのバリデーション
-  validates :post_code, presence: true, format:{with: /\A\d{3}[-]\d{4}\z/}
-  validates :phon_number, presence: true, format:{with: /\A\d{10,11}\z/}
+with_options presence: true do
+  validates  :post_code,      format:{with: /\A\d{3}[-]\d{4}\z/}
+  validates  :phone_number,   format:{with: /\A\d{10,11}\z/}
+  validates  :user_id
+end
 
 #orderとaddressへのデータ保存
   def save
-    order = Order.create(user_id: current_user.id, item_id: @item.id)
-    Address.create(post_code:post_code,prefecture_id:prefecture_id,mailing_address:mailing_address,house_number:house_number,building_name:building_name,phone_number:phone_number,order_id:order.id)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, mailing_address: mailing_address, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
