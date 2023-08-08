66584978bd1a79b5ab8b9ad7da295910b3ef4c9c
hiroaki9842 <137044356+hiroaki9842@users.noreply.github.com>
2023-08-08 10:31:03 +0900

    ordersコントローラー作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,27 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+
+  def index
+      @item = Item.find(params[:item_id])
+      if user_signed_in? && current_user.id == @item.user_id
+        redirect_to root_path
+      end
+      @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      return redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:post_code,:prefecture_id,:mailing_address,:house_number,:building_name,:phone_number).merge(item_id: @item.id, user_id: @item.user_id)
+  end
+end
