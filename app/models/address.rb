0cb23558aba4bf6b7300891b88f5b6adda54f3a6
hiroaki9842 <137044356+hiroaki9842@users.noreply.github.com>
2023-08-08 15:27:10 +0900

    addressモデル修正#1

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,7 +1,3 @@
 class Address < ApplicationRecord
-  has_one_active_hash :prefecture_id
   belongs_to :order
-
-  # validates :post_code, presence: true, format:{with: /\A\d{3}[-]\d{4}\z/}
-  # validates :phon_number, presence: true, format:{with: /\A\d{10,11}\z/}
 end
